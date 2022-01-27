e4afd261f2e91b7b65a69b34a974af5025cb5ba4
Hafiz Elfia Wedo Putra <52981264+hafizewp22@users.noreply.github.com>
2022-01-27 12:31:29 +0700

    Update OrderController.php

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\order;
+use App\Models\Order;
 
 class OrderController extends Controller
 {
     public function index()
     {
-        $orders = order::where('status', '0')->get();
+        $orders = Order::where('status', '0')->get();
         return view('admin.orders.index', compact('orders'));
     }
 
